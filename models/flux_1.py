c79fdc469de2b852408721d8facb020a194fa093
amr <amr@epieos.com>
2024-08-11 19:20:48 +0900

    fix

--- models/flux_1.py
+++ models/flux_1.py
@@ -5,15 +5,16 @@ from diffusers import FluxPipeline
 def generate_image(prompt: str):
     
     pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-schnell", torch_dtype=torch.bfloat16)
+    pipe.to("cuda")
     
     image_store = io.BytesIO()
-    images = pipe(
+    image = pipe(
         prompt,
         guidance_scale=0.0,
         num_inference_steps=4,
         max_sequence_length=256,
-        generator=torch.Generator("cuda").manual_seed(0)
+        generator=torch.Generator("cpu").manual_seed(0)
     ).images[0]
-    images[0].save(image_store, "PNG")
+    image.save(image_store, "PNG")
     
     return image_store.getvalue()
